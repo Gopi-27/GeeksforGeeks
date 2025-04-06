784623e90fe76d347554e3eec14c5196847e54ec
Gopi <gopiyadav4788@gmail.com>
2025-04-06 20:21:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        printf("%d ", num);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to sort an array using quick sort algorithm.
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low >= high)return ;
+        int pidx = partition(arr,low,high);
+        quickSort(arr,low, pidx - 1);
+        quickSort(arr,pidx + 1,high);
+        return;
+    }
+
+  public:
+    // Function that takes last element as pivot, places the pivot element at
+    // its correct position in sorted array, and places all smaller elements
+    // to left of pivot and all greater elements to right of pivot.
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        int l = low;
+        int h = high;
+        int pivot = arr[low];
+        while(l < h){
+            while(l <= high && arr[l] <= pivot)l++;
+            while(arr[h] > pivot)h--;
+            if(l < h)swap(arr[l],arr[h]);
+        }
+        swap(arr[low],arr[h]);
+        return h;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    scanf("%d", &T);
+    getchar(); // to consume newline after T
+
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, arr.size() - 1);
+        printArray(arr);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
