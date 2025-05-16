1e17cc5cf2049b765593f753e8a07992c1faf190
Gopi <gopiyadav4788@gmail.com>
2025-05-16 10:45:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Odd and Even/count-odd-and-even.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    /*Function to count even and odd elements in the array
+     * arr : Array with its elements
+     */
+    pair<int, int> countOddEven(vector<int> &arr) {
+        // your code here
+        pair<int,int>Ans;
+        int first = 0;
+        int second = 0;
+        for(int i : arr){
+            if(i % 2 == 0)second++;
+            else first++;
+        }
+        Ans.first = first;
+        Ans.second = second;
+        return Ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    // Testcase input
+    int testcase;
+    cin >> testcase;
+    cin.ignore();
+
+    while (testcase--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+
+        pair<int, int> res = ob.countOddEven(arr);
+        cout << res.first << " " << res.second << " ";
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
