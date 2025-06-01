93244c19a501fd23554053a2b1b945fb7f47c572
Gopi <gopiyadav4788@gmail.com>
2025-06-01 20:11:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(int n) {
+        // code here
+        if(n == 0)return false;
+        if((n & (n - 1)) == 0)return true;
+        return false;
+    }
+};
