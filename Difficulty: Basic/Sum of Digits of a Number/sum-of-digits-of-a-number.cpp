5fd1d9052abd61da425e3e62301b1d4696ce1afd
Gopi <gopiyadav4788@gmail.com>
2025-07-30 16:41:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Digits of a Number/sum-of-digits-of-a-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+        // Code here
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+};
