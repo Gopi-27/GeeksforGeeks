4f5909105402de9f5817c7339e5de960cb9243ba
Gopi <gopiyadav4788@gmail.com>
2026-02-17 10:21:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -3,35 +3,22 @@ class Solution {
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
         int n = val.size();
-        //        1kg value, avail wt
-        vector<pair<double,int>>perKg(n);
-        
+        vector<pair<double,int>>A(n);
         for(int i = 0; i < n; i++){
-            perKg[i] = {((double)val[i])/wt[i],wt[i]};
+            A[i].first = (double)val[i]/(double)wt[i];
+            A[i].second = wt[i];
         }
-        
-        sort(perKg.begin(),perKg.end());
-        
-        int idx = n - 1;
+        sort(A.begin(),A.end(),greater<pair<double,int>>());
         double ans = 0;
         
-        while(capacity > 0 && idx >= 0){
-            
-            if(capacity >= perKg[idx].second){
-                
-                ans += perKg[idx].first * perKg[idx].second;
-                
-                capacity -= perKg[idx].second;
-                
-            }else{
-                
-                ans += perKg[idx].first * capacity;
-                return ans;
-                
-            }
-            
-            idx--;
-            
+        for(int i = 0; i < n; i++){
+           if(capacity >= A[i].second){
+               ans += A[i].first * A[i].second;
+               capacity -= A[i].second;
+           }else{
+               ans += capacity * A[i].first;
+               return ans;
+           }
         }
         return ans;
     }
